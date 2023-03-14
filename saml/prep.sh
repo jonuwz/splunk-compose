e7c95a0ccd54cfb008da569e4f9b48ca16feadd0
John Hughes <jonuwz@gmail.com>
2023-03-14 22:39:37 +0000

    removed temp stuff

--- saml/prep.sh
+++ saml/prep.sh
@@ -4,8 +4,8 @@ rm -rf resources/certs
 mkdir -p resources/certs
 openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj "/CN=apps,OU=iam,DC=home,DC=local" -keyout resources/certs/keycloakapps.key -out resources/certs/keycloakapps.crt
 openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj "/CN=splunksaml,OU=iam,DC=home,DC=local" -keyout resources/certs/splunksaml.key -out resources/certs/splunksaml.crt
-cat resources/certs/splunksaml.crt resources/certs/splunksaml.key >  resources/splunk/system/local/splunksaml.pem
-cp resources/certs/keycloakapps.crt resources/splunk/system/local/idpCerts
+cat resources/certs/splunksaml.crt resources/certs/splunksaml.key >  resources/splunk/splunk-etc/auth/splunksaml.pem
+cp resources/certs/keycloakapps.crt resources/splunk/splunk-etc/auth/idpCerts
 
 SPLUNKSAMLCERTIFICATE=$(openssl x509 -in resources/certs/splunksaml.crt -outform der | base64 -w 0)
 APPSCERTIFICATE=$(awk '{printf("%s\\\\n",$0)}' resources/certs/keycloakapps.crt)
